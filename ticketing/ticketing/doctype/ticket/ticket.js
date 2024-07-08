8ed04bd1b5e0ab8d80d85cfa64677d383f23bd7f
sujit <ssutar@dexciss.com>
2024-07-09 01:29:51 +0530

    some progress

--- ticketing/ticketing/doctype/ticket/ticket.js
+++ ticketing/ticketing/doctype/ticket/ticket.js
@@ -2,9 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Ticket", {
-	// refresh(frm) {
-
-	// },
+	refresh(frm) {
+        createButton(frm)
+	},
+    after_save:function(frm){
+        createButton(frm)
+    },
     service_requested:function(frm){
         frappe.call({
             method:"get_contract",
@@ -12,10 +15,59 @@ frappe.ui.form.on("Ticket", {
             freeze:true,
             freeze_message:"Fetching contract",
             callback: function(r, rt){
+                console.log("r.message=",r);
+                
                 if(r.message){
-                    frm.set_value("contract", r.message);
+                    frm.set_value("contract", r.message.name);
+                    if (['In Progress', 'Expiring'].includes(r.message.status)){
+                        frm.set_value("contract_status", "Active");
+                    }else{
+                        frm.set_value("contract_status", "Inactive");
+                    }
+                }else{
+                    frappe.msgprint("No contract found for this ticket.");
+                    frm.set_value("contract", null);
+                    createButton(frm)
                 }
             }
         })
     }
 });
+
+
+function createButton(frm, status=undefined) {
+    if (frm.doc.__islocal) {
+        return
+    }
+    console.log("createButton", status)
+    if (frm.doc.contract){
+        frappe.db.get_value('Customer Contract', frm.doc.contract,'status')
+        .then(val => {
+            console.log('doc',val)
+            if(['Expired'].includes(val.message.status)){
+                console.log("inside if createButton")
+                frm.add_custom_button(__("Create Opportunity"), function() {
+                    
+                }, "Create");
+            }else{
+                frm.remove_custom_button("Create","Create Opportunity");
+            }
+        })
+    }else{
+        frm.add_custom_button(__("Create Opportunity"), function() {
+                    
+        }, "Create");
+    }
+
+   
+    frm.add_custom_button(__("Create Service"), function() {
+        
+    }, "Create");
+    frm.add_custom_button(__("Create Ticket Invoice"), function() {
+        
+    }, "Create");
+    frm.add_custom_button(__("Create Service Ticket Invoice"), function() {
+        
+    }, "Create");
+    
+}
--- ticketing/ticketing/doctype/ticket/ticket.py
+++ ticketing/ticketing/doctype/ticket/ticket.py
@@ -17,18 +17,22 @@ from ticketing.api import get_sla
 
 
 class Ticket(Document):
-	
 	def validate(self):
-		pass
-		# if self.is_new():
-		# 	self.service_level_agreement_creation=
-		# if self.link_yrud:
-		# 	sla=frappe.get_doc("Service Level Agreement",self.link_yrud)
-		# 	get_sla(self,sla)
+		if self.contract:
+			print(f"""select tk.name from `tabTicket` as tk join `tabCustomer Contract` as cc on tk.contract=cc.name where tk.creation BETWEEN cc.from_date AND cc.to_date and cc.customer='{self.customer}' AND cc.name='{self.contract}';
+			""")
+			already_ticket=frappe.db.sql(f"""select tk.name from `tabTicket` as tk join `tabCustomer Contract` as cc on tk.contract=cc.name where tk.creation BETWEEN cc.from_date AND cc.to_date and cc.customer='{self.customer}' AND cc.name='{self.contract}';
+			""",pluck=True)
+		# if already_ticket:
+		# 	frappe.throw(f"A ticket already exists for the customer {self.customer} with contract {self.contract}")
 	
 	@frappe.whitelist()
 	def get_contract(self):
-		print("get_contract")
-		contract=frappe.db.sql(f"""select cc.name from `tabCustomer Contract` as cc join `tabContract Address Items` as addr on cc.name = addr.parent join `tabContract Covered Services` as sc on cc.name= sc.parent where addr.customer_address ='{self.customer_address}' and sc.service='{self.service_requested}' ORDER BY cc.creation DESC LIMIT 1;""",pluck=True)
-		print("contract",contract)
-		return contract
+		print("get_contract---"+f"""select cc.name,cc.status from `tabCustomer Contract` as cc join `tabContract Address Items` as addr on cc.name = addr.parent join `tabContract Covered Services` as sc on cc.name= sc.parent where addr.customer_address ='{self.customer_address}' and sc.service='{self.service_requested}' ORDER BY cc.creation DESC LIMIT 1;""")
+		data=frappe.db.sql(f"""select cc.name,cc.status from `tabCustomer Contract` as cc join `tabContract Address Items` as addr on cc.name = addr.parent join `tabContract Covered Services` as sc on cc.name= sc.parent where addr.customer_address ='{self.customer_address}' and sc.service='{self.service_requested}' and cc.customer='{self.customer}' ORDER BY cc.creation DESC LIMIT 1;""",as_dict=True)
+		print("data sql",data)
+		if data:
+			return data[0]
+		else:
+			return False
+		return '4oefv9k1um'
