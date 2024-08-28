ca2584ebc506ab14ca98eb19a0bd1486e9775953
sujit <ssutar@dexciss.com>
2024-08-28 20:02:21 +0530

    web form

---
+++ ticketing/ticket_wform_api.py
@@ -0,0 +1,54 @@
+import frappe
+
+@frappe.whitelist()
+def check_warranty(equipment,customer,customer_address):
+    print("check warranty")
+    print(f"""select we.status ,w.name from `tabWarranty` as w join `tabWarranty Address` as wa on w.name=wa.parent join `tabWarranty Equipments` as we on w.name=we.parent where we.equipment="{equipment}" and w.customer="{customer}" and wa.warranty_address="{customer_address}" order by w.creation desc;""")
+    data=frappe.db.sql(f"""select we.status ,w.name from `tabWarranty` as w join `tabWarranty Address` as wa on w.name=wa.parent join `tabWarranty Equipments` as we on w.name=we.parent where we.equipment="{equipment}" and w.customer="{customer}" and wa.warranty_address="{customer_address}" order by w.creation desc;""",as_dict=True)
+    print("data",data)
+    return_data={}
+    if data:
+        data=data[0]
+        if data.get('status') == 'Active' or data.get('status') == 'Expiring':
+            return_data['warranty_status'] = "Under Warranty"
+        elif data.get('status') == 'Expired':
+            return_data['warranty_status'] = "Expired"
+        else:
+            return_data['warranty_status'] = "No Warranty"
+        return_data['warranty']=data.get('name')
+    else:
+        return_data['warranty_status'] = "No Warranty"
+        return_data['warranty'] = None
+    print("return data",return_data)
+    return return_data
+
+
+@frappe.whitelist()
+def get_contract(service_requested,customer,customer_address):
+    print("get_contract---"+f"""select cc.name,cc.status from `tabCustomer Contract` as cc join `tabContract Address Items` as addr on cc.name = addr.parent join `tabContract Covered Services` as sc on cc.name= sc.parent where addr.customer_address ='{customer_address}' and sc.service='{service_requested}' ORDER BY cc.creation DESC LIMIT 1;""")
+    data=frappe.db.sql(f"""select cc.name,cc.status from `tabCustomer Contract` as cc join `tabContract Address Items` as addr on cc.name = addr.parent join `tabContract Covered Services` as sc on cc.name= sc.parent where addr.customer_address ='{customer_address}' and sc.service='{service_requested}' and cc.customer='{customer}' ORDER BY cc.creation DESC LIMIT 1;""",as_dict=True)
+    print("data sql",data)
+    if data:
+        return data[0]
+    else:
+        return False
+    
+@frappe.whitelist()
+def get_customer_address(customer):
+    
+    data=frappe.db.get_all("Address",[["link_name","=",customer]],pluck='name')
+    print("data sql",data)
+    if data:
+        return data
+    else:
+        return []
+    
+@frappe.whitelist()
+def get_customer(user):
+    
+    data=frappe.db.get_all("Customer",[["user","=",user]],pluck='name')
+    print("data sql",data)
+    if data:
+        return data
+    else:
+        return []
--- ticketing/ticketing/web_form/ticket/ticket.js
+++ ticketing/ticketing/web_form/ticket/ticket.js
@@ -1,4 +1,6 @@
 frappe.ready(function() {
+
+    
 	// bind events here
 	frappe.web_form.on('customer_address', async () =>{
 		frappe.call({
@@ -10,5 +12,121 @@ frappe.ready(function() {
                 frappe.web_form.set_value('customer_address_display', r.message);
             }
 		})
+        clearAll()
 	});
+
+    frappe.web_form.on('equipment', async () =>{
+        console.log("equipment",frappe.web_form.get_value('equipment'))
+        if (!frappe.web_form.get_value('equipment')){
+            console.log("no equipment request data")
+            return 
+        }
+		frappe.call({
+			method: 'ticketing.ticket_wform_api.check_warranty',
+            args: {
+                equipment: frappe.web_form.get_value('equipment'),
+                customer: frappe.web_form.get_value('customer'),
+                customer_address: frappe.web_form.get_value('customer_address')
+            },
+            callback: function(r) {
+                console.log("res equipent",r)
+                frappe.web_form.set_value('warranty', r.message.warranty);
+                frappe.web_form.set_value('warranty_status', r.message.warranty_status);
+                frappe.web_form.set_df_property('warranty', 'read_only',true);
+                if (r.message.warranty_status == 'No Warranty'){
+                    frappe.web_form.set_df_property('warranty', 'hidden',true);
+                }else{
+                    frappe.web_form.set_df_property('warranty', 'hidden',false);
+                }
+            }
+		})
+	});
+
+    frappe.web_form.on('service_requested', async () =>{
+        console.log("service requested",frappe.web_form.get_value('service_requested'))
+        if (!frappe.web_form.get_value('service_requested')){
+            console.log("no service request data")
+            return 
+        }
+		frappe.call({
+			method: 'ticketing.ticket_wform_api.get_contract',
+            args: {
+                service_requested: frappe.web_form.get_value('service_requested'),
+                customer: frappe.web_form.get_value('customer'),
+                customer_address: frappe.web_form.get_value('customer_address')
+            },
+            callback: function(r) {
+                console.log("res equipent",r)
+                if (!r.message){
+                    frappe.web_form.set_value('contract_status', 'Not Found');
+                    frappe.web_form.set_value('contract', undefined);
+                    frappe.web_form.set_df_property('contract', 'hidden',true);
+                    
+                }
+                else if (['In Progress', 'Expiring'].includes(r.message.status)){
+                    frappe.web_form.set_value('contract', r.message.name);
+                    frappe.web_form.set_value('contract_status', 'Active');
+                    frappe.web_form.set_df_property('contract', 'hidden',false);
+                    frappe.web_form.set_df_property('contract', 'read_only',true);
+                }else{
+                    frappe.web_form.set_value('contract_status', 'Not Found');
+                    frappe.web_form.set_value('contract', undefined);
+                }
+                // if (!r.message){
+                //     frappe.web_form.set_value('warranty_status', 'Not Found');
+                //     frappe.web_form.set_value('contract', undefined);
+                // }else{
+                    
+                //     frappe.web_form.set_value('contract', r.message.name);
+                //     frappe.web_form.set_value('contract_status', r.message.status);
+                // }
+            }
+		})
+	});
+
+    frappe.web_form.on('ticket_type', async () =>{
+        clearAll()
+	});
+
+    frappe.web_form.on('customer', () =>{
+        fetchCustomerAddress()
+	});
+
+    function clearAll(){
+        frappe.web_form.set_value('warranty', undefined);
+        frappe.web_form.set_value('warranty_status', undefined);
+        frappe.web_form.set_value('service_requested', undefined);
+        frappe.web_form.set_value('contract_status',undefined);
+        frappe.web_form.set_value('contract', undefined);
+        frappe.web_form.set_value('equipment', undefined);   
+    }
+
+    console.log("onload script")
+    function fetchCustomerAddress(){
+        frappe.call({
+            method: 'ticketing.ticket_wform_api.get_customer_address',
+            args: {
+                customer: frappe.web_form.get_value('customer')
+            },
+            callback: function(r) {
+                console.log("res address",r)
+                frappe.web_form.fields_dict.customer_address.set_data(r.message)
+            }
+        })
+    }
+
+    function fetchCustomer(){
+        frappe.call({
+            method: 'ticketing.ticket_wform_api.get_customer',
+            args: {
+                user: frappe.session.user
+            },
+            callback: function(r) {
+                console.log("res address",r)
+                frappe.web_form.fields_dict.customer.set_data(r.message)
+            }
+        })
+    }
+    fetchCustomer()
 })
+
