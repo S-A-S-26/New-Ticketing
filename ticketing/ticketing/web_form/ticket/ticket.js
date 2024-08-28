1e89e13ec96381fbbd221c177a84c32335f27ba3
sujit <ssutar@dexciss.com>
2024-08-28 11:11:47 +0530

    web form script

--- ticketing/ticketing/web_form/ticket/ticket.js
+++ ticketing/ticketing/web_form/ticket/ticket.js
@@ -1,3 +1,14 @@
 frappe.ready(function() {
 	// bind events here
+	frappe.web_form.on('customer_address', async () =>{
+		frappe.call({
+			method: 'frappe.contacts.doctype.address.address.get_address_display',
+            args: {
+                address_dict: frappe.web_form.get_value('customer_address')
+            },
+            callback: function(r) {
+                frappe.web_form.set_value('customer_address_display', r.message);
+            }
+		})
+	});
 })
